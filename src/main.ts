59e1b52efde261049bb3fa0dcc0d39bd664cd027
muhammadharis1212 <muhammadharis1212@gmail.com>
2022-12-04 18:36:59 +0500

    started project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Weaving-ERP')
+    .setDescription('API for weaving factory management')
+    .setVersion('1.0')
+    .addTag('textile')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(5000);
+}
+bootstrap();
